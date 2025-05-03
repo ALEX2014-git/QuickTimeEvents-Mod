5efb3c722ffbf17ef6e767575a63002d9323341f
ALEX2014 <alekseev.lexa04@gmail.com>
2025-05-03 20:47:01 +0300

    Implemented basic ButtonSequence graphics

--- Options.cs
+++ Options.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using BepInEx.Logging;
 using Menu.Remix.MixedUI;
 using Menu.Remix.MixedUI.ValueTypes;
@@ -14,12 +15,15 @@ public class PluginOptions : OptionInterface
         Logger = loggerSource;
         DisableLizzardRNG = this.config.Bind<bool>("DisableLizzardRNG", true);
         PunishFailure = this.config.Bind<bool>("PunishFailure", false, new ConfigurableInfo("If enabled, kills slugcat of the player who failed QTE. Disabled by default."));
+        TimeSlowMode = this.config.Bind<string>("TimeSlowMode", "Stop");
     }
 
     public readonly Configurable<bool> DisableLizzardRNG;
     public readonly Configurable<bool> PunishFailure;
-
+    public readonly Configurable<string> TimeSlowMode;
     private UIelement[] UIArrGeneral;
+    private static readonly string[] TimeSlowModeArr = { "Stop", "Slow" };
+    OpComboBox timeSlowComboBox;
 
 
     public override void Initialize()
@@ -30,6 +34,7 @@ public class PluginOptions : OptionInterface
         {
             opTab
         };
+        timeSlowComboBox = new OpComboBox(TimeSlowMode, new Vector2(120f, 400f), 80f, TimeSlowModeArr);
 
         UIArrGeneral = new UIelement[]
         {
@@ -37,14 +42,16 @@ public class PluginOptions : OptionInterface
             new OpLabel(10f, 520f, "Disable Lizzard bite RNG death"),
             new OpCheckBox(DisableLizzardRNG, 10f, 490f),
             new OpLabel(10f, 460f, "Punish QTE failure"),
-            new OpCheckBox(PunishFailure, 10f, 430f){ description = PunishFailure.info.description }
+            new OpCheckBox(PunishFailure, 10f, 430f){ description = PunishFailure.info.description },
+            new OpLabel(10f, 400f, "Select time behavior during QTE"),
+            timeSlowComboBox
         };
         opTab.AddItems(UIArrGeneral);
     }
 
     public override void Update()
     {
-        //UIArrDebug[2].;
+        
     }
 
 }
